95d134631b584cc6026e963e5fcfc0fe0164051b
clarklight <clarkswork@gmail.com>
2015-11-18 12:43:48 +0800

    Update MifarePlugin.java

--- src/android/MifarePlugin.java
+++ src/android/MifarePlugin.java
@@ -17,6 +17,7 @@ import com.nxp.nfclib.utils.Utilities;
 import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
 import com.nxp.nfcliblite.Interface.Nxpnfcliblitecallback;
 import com.nxp.nfcliblite.cards.DESFire;
+import com.nxp.nfcliblite.cards.IDESFireEV1;
 import com.nxp.nfcliblite.cards.Plus;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
